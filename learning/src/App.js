7128dc9c9f709172e28c8ebc358f9ce429a9a280
Dinesh Kumar Gajjela <sanjay.gajjala1995@gmail.com>
2024-02-26 06:56:22 +0530

    added Class Component

--- learning/src/App.js
+++ learning/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Hello from './components/functional/hello'
+import HelloC from './components/classes/HelloC'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
          <div> Hello World App </div>
         
       <Hello />
+      <HelloC />
 
     </div>
   );
---
+++ learning/src/components/classes/HelloC.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react'
+
+class HelloC extends Component {
+    render() {
+        return <h1>Hello Class</h1>
+    }
+}
+
+export default HelloC
