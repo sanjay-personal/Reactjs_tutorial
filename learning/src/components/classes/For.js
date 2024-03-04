8730d3e135e8b59392bb25871d45d3b4dcc15826
Dinesh Kumar Gajjela <sanjay.gajjala1995@gmail.com>
2024-03-04 06:27:46 +0530

    added For loop another way displaying

--- learning/src/components/classes/For.js
+++ learning/src/components/classes/For.js
@@ -6,8 +6,12 @@ class For extends Component {
 
     }
     render() {
-        const names = ['Hello1','Hello2','Hello3']
-        return <div>{names.map(name=><h1>{name}</h1>)}</div> 
+        // const names = ['Hello1','Hello2','Hello3'];
+        // return <div>{names.map(name=><h1>{name}</h1>)}</div> 
+
+        const names = ['Hello1','Hello2','Hello3'];
+        const name = names.map(name=><h1>{name}</h1>)
+        return <div>{name}</div>
     }
 }
 
